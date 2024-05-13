5f85ee91aaac863e729bbb9d873321dca911927d
Scott <hscottvo@gmail.com>
2024-05-13 02:41:33 -0700

    Initial commit plugins

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,11 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+-- print 'Lua configuration loaded successfully!'
+return {
+  require 'custom/plugins/noice',
+  require 'custom/plugins/quickscope',
+  require 'custom/plugins/lazygit',
+  require 'custom/plugins/neo-tree',
+  require 'custom/plugins/catppuccin',
+}
